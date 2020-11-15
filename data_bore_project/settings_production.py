4fa5a833d929bfa089bc55e67463491d552bd26d
Jackson Price <jdprice1995@gmail.com>
2020-11-14 18:21:36 -0600

    commit files for heroku deployment

--- data_bore_project/settings_production.py
+++ data_bore_project/settings_production.py
@@ -1,6 +1,7 @@
 from data_bore_project.settings import *
 
 import dj_database_url
+import django_heroku
 
 DATABASES['default'] = dj_database_url.config()
 
@@ -9,3 +10,5 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+django_heroku.settings(locals())
